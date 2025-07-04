b9cbbbdeb1cd083a86fbe31366ef94371adeaa05
Preetham Reddy <Preethamreddy2808@gmail.com>
2025-07-04 21:40:32 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
-from flask import Flask, request, render_template
-from datetime import datetime
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
     name = request.form.get('name')
     email = request.form.get('email')
     message = request.form.get('message')
-
     with open("submissions.txt", "a") as f:
-        f.write(f"{datetime.now()} | Name: {name}, Email: {email}, Message: {message}\n")
-
-    return "<h2>Thanks for your message!</h2><a href='/'>Back to site</a>"
+        f.write(f"{name} | {email} | {message}\n")
+    return "Thank you for contacting!"
 
 if __name__ == "__main__":
     app.run(debug=True)
